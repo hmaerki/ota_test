25ae4798bac2cbb8b30b89b9f136991776ad1816
Hans Maerki <hans@maerki.com>
2018-12-04 01:34:09 +0100

    ok

--- software/http_server/python/config_nodes.py
+++ software/http_server/python/config_nodes.py
@@ -1,27 +1,24 @@
 # -*- coding: utf-8 -*-
 
-MAC = 'strMac'
-NAME = 'strName'
-LAB_NAME = 'strLabName'
-RESPONSIBLE = 'strResposible'
-GIT_TAGS = 'strGitTags'
-USER_TAG = 'strUserTag'
-NODES = 'listNodes'
+from python3_config_nodes_lib import *
 
-dictNodes = {}
-dictLabs = {}
+listNodes = []
+listLabs = []
 
-dictNodes['20180907_01'] = {
+listNodes.append({
+  SERIAL: '20180907_01',
   MAC: '3C71BF0F97A4',
   NAME: 'steel',
-}
+})
 
-dictNodes['20180907_03'] = {
+listNodes.append({
+  SERIAL: '20180907_03',
   MAC: '840D8E1BC40C',
   NAME: 'aluminium',
-}
+})
 
-dictLabs['labHombi'] = {
+listLabs.append({
+  LAB_LABEL: 'labHombi',
   LAB_NAME: 'hombrechtikon',
   RESPONSIBLE: 'Peter Maerki',
   GIT_TAGS: 'x1.0;y1.1',
@@ -29,9 +26,11 @@ dictLabs['labHombi'] = {
   NODES: (
     '20180907_01',
   )
-}
+})
 
-dictLabs['labY'] = {
+
+listLabs.append({
+  LAB_LABEL: 'labY',
   LAB_NAME: 'ETH, LabY',
   RESPONSIBLE: 'Robin',
   GIT_TAGS: 'y1.1;x1.0',
@@ -39,17 +38,38 @@ dictLabs['labY'] = {
   NODES: (
     '20180907_03',
   )
+})
+
+dictConfigNodes = {
+  LIST_LABS: listLabs,
+  LIST_NODES: listNodes,
 }
 
 if __name__ == '__main__':
-  # Test if the configuration may collect all files from github.
   import python3_github_pull
-  for strLab, dictLab in dictLabs.items():
+
+  # Test if the configuration may collect all files from github.
+  
+  objConfigNodes = ConfigNodes(dictConfigNodes)
+  objConfigNodes.verifyConsistency()
+  if True:
+    p = python3_github_pull.GithubPull(strDirectory='.')
+    p.setMac('840D8E1BC40C')
+    strTarFilenameFull = p.getTar()
+
+  if False:
+    for strMac in sorted(dictMacs.keys):
+      dictMac = dictMacs[strMAC]
+      print('  %s: "%s" %s %s' % (strMac, dictMac[LAB_NAME], dictMac[GIT_TAGS], dictMac[USER_TAG]))
+
+  for dictLab in listLabs:
     try:
-      p = python3_github_pull.GithubPull(strDirectory='.', strGitTags=dictLab[GIT_TAGS], strUserTag=dictLab[USER_TAG])
+      p = python3_github_pull.GithubPull(strDirectory='.')
+      p.setTags(dictLab[GIT_TAGS], strUserTag=dictLab[USER_TAG])
       strTarFilenameFull = p.getTar()
-      print('Lab "%s": %s' % (strLab, strTarFilenameFull))
+      print('Lab "%s": %s' % (dictLab[LAB_LABEL], strTarFilenameFull))
     except Exception:
-      print('ERROR in Lab "%s"' % strLab)
+      print('ERROR in Lab "%s"' % dictLab[LAB_LABEL])
       raise
+
 
